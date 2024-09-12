c8444c531d9322393d366210b6f37d09df9ed3df
Michael Küchenmeister <michaelkuechenmeister95@googlemail.com>
2024-09-12 19:49:27 +0200

    navbar, sidebar implemented

--- portfolio-website/app/layout.jsx
+++ portfolio-website/app/layout.jsx
@@ -1,6 +1,11 @@
 import { Exo_2 } from "next/font/google";
 import "./globals.css";
 
+// components
+import Header from "@/components/Header";
+import PageTransition from "@/components/PageTransition";
+import StairTransition from "@/components/StairTransition";
+
 const exo2 = Exo_2({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
@@ -15,10 +20,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${exo2.variable} antialiased`}
-      >
-        {children}
+      <body className={`${exo2.variable} antialiased`}>
+        <Header />
+        <StairTransition />
+        <PageTransition>{children}</PageTransition>
       </body>
     </html>
   );
--- portfolio-website/app/page.jsx
+++ portfolio-website/app/page.jsx
@@ -1,7 +1,53 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
 
-export default function Home() {
+// components
+import Social from "@/components/Social";
+
+const Home = () => {
   return (
-    <main>Homepage</main>
-  );
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          {/* text */}
+          <div className="text-center xl:text-left">
+            <span className="text-xl">Software Developer & Master Student</span>
+            <h1 className="h1 mb-6">
+              Hallo ich bin <br /> <span className="text-accent">Michael Küchenmeister</span>
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, 
+              sed diam nonumy eirmod tempor invidunt ut labore et dolore 
+              magna aliquyam
+            </p>
+            {/* btn and socials */}
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              <Button
+                variant="outline"
+                size="lg"
+                className="uppercase flex items-center gap-2"
+              >
+                <span>Download CV</span>
+                <FiDownload className="text-xl"/>
+              </Button>
+              <div className="mb-8 xl:mb-0">
+                <Social 
+                  containerStyles="flex gap-6" 
+                  iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center 
+                  items-center text-accent text-base hover:bg-accent hover:text-primary 
+                  hover:transition-all duration-500"
+                />
+              </div>
+            </div>
+          </div>
+          {/* photo */}
+          <div>
+            photo
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
+
+export default Home
